538a0b14add2294185aeea3b9f9ae4ceabd8f084
David Wee <rook2pawn@gmail.com>
2015-12-24 05:54:57 -0800

    merge fastforward

---
+++ test/test-merge-fastforward.js
@@ -0,0 +1,21 @@
+var gitjson = require('../index.js')
+var git = gitjson()
+var assert = require('assert')
+
+git.init()
+git.save('mydocument', {foo:'bar', val:2})
+git.add('mydocument')
+git.commit('first commit')
+
+git.branch('test')
+git.checkout('test')
+git.save('mydocument',{foo:'bar',val:3})
+git.add('mydocument')
+git.commit('added one to val to make it 3')
+
+git.log()
+//assert.equal(true,git.ancestor('master'))
+console.log(git.ancestor('master'))
+git.merge('master')
+git.log()
+assert.equal(git.refs['test'], git.refs['master'])
